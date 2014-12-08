e3c6cd2a1d9343980fa5cfba49a4b20c6cab3abb
Joao Paulo Targino <joaotargino@gmail.com>
2014-12-08 11:11:54 -0300

    dialog com (des)confirmar presenca+detalhes

--- CiaDoBusao/src/com/application/ciadobusao/telas/MeusEncontrosFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/MeusEncontrosFragment.java
@@ -6,6 +6,8 @@ import com.application.ciadobusao.R;
 import com.application.ciadobusao.db.SingletonDB;
 import com.application.ciadobusao.util.Encontro;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -32,13 +34,15 @@ public class MeusEncontrosFragment extends Fragment {
 				container, false);
 
 		// stringArray = new String[15];
-//		 for(int i=0; i < SingletonDB.getInstance().getEncontros().size(); i++){
-//		 meusEncontros.add();
-//		 }
+		// for(int i=0; i < SingletonDB.getInstance().getEncontros().size();
+		// i++){
+		// meusEncontros.add();
+		// }
 
-//		meusEncontros = SingletonDB.getInstance().getEncontros();
+		// meusEncontros = SingletonDB.getInstance().getEncontros();
 		tweetItemArrayAdapter = new ArrayAdapter(this.getActivity(),
-				android.R.layout.simple_list_item_1, meusEncontros.getEncontros());
+				android.R.layout.simple_list_item_1,
+				meusEncontros.getEncontros());
 		tweetListView = (ListView) rootView.findViewById(R.id.encontros);
 		tweetListView.setAdapter(tweetItemArrayAdapter);
 
@@ -50,25 +54,76 @@ public class MeusEncontrosFragment extends Fragment {
 		// ArrayAdapter<String>(getActivity(),
 		// android.R.layout.simple_list_item_1, values);
 		// tweetListView.setAdapter(adapter);
-		
+
 		tweetListView.setOnItemClickListener(new OnItemClickListener() {
-		 @Override
-		  public void onItemClick(AdapterView<?> parent, View view,
-		    int position, long id) {
-//			  ImageView imageView = (ImageView) view.findViewById(R.id.icon);
-				  try {
-					  meusEncontros.delEncontroAtIndex(position);
-				} catch (Exception e) {
-					// TODO: handle exception
-				} 
-		    Toast.makeText(getActivity().getApplicationContext(),
-		      "Encontro " + position + " removido" , Toast.LENGTH_LONG)
-		      .show();
-		    tweetItemArrayAdapter.notifyDataSetChanged();
-		  }
-		}); 
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				final int pos = position;
+				AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+						.create(); // Read Update
+				alertDialog.setTitle(meusEncontros.getEncontros().get(position)
+						.getNome());
+				alertDialog
+						.setMessage(meusEncontros.getEncontros().get(position)
+								.getPonto()
+								+ "\n"
+								+ meusEncontros.getEncontros().get(position)
+										.getLinha()
+								+ "\n"
+								+ meusEncontros.getEncontros().get(position)
+										.getData()
+								+ "\n"
+								+ meusEncontros.getEncontros().get(position)
+										.getHorario()
+								+ "\n"
+								+ "confirmados: \n"
+								+ "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.");
+
+				alertDialog.setButton("Desconfirmar",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								try {
+									meusEncontros.delEncontroAtIndex(pos);
+								} catch (Exception e) {
+									// TODO: handle exception
+								}
+								tweetItemArrayAdapter.notifyDataSetChanged();
+							}
+						});
+				alertDialog.setButton2("Fechar",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// simplesmente fecha a janela
+							}
+						});
+				// alertDialog.setButton3("Editar", new
+				// DialogInterface.OnClickListener() {
+				// public void onClick(DialogInterface dialog, int which) {
+				// // ir pra ediçao talvez
+				// }
+				// });
+
+				alertDialog.show();
+
+				// ImageView imageView = (ImageView)
+				// view.findViewById(R.id.icon);
+				// try {
+				// meusEncontros.delEncontroAtIndex(position);
+				// } catch (Exception e) {
+				// // TODO: handle exception
+				// }
+				// Toast.makeText(getActivity().getApplicationContext(),
+				// "Encontro " + position + " removido" , Toast.LENGTH_LONG)
+				// .show();
+				// tweetItemArrayAdapter.notifyDataSetChanged();
+			}
+		});
 
 		return rootView;
 	}
-	
+
 }
--- CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
@@ -9,6 +9,8 @@ import com.application.ciadobusao.util.Encontro;
 import com.application.ciadobusao.util.HorarioDoEncontro;
 //import com.application.ciadobusao.util.NotificacoesAdapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,64 +27,102 @@ import android.widget.Toast;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
-
-public class NotificacoesFragment extends Fragment{
+public class NotificacoesFragment extends Fragment {
 	private ListView tweetListView;
-    private ArrayList<Encontro> notificacoes ;
-    private ArrayAdapter tweetItemArrayAdapter;
-    private HorarioDoEncontro horario;
-    private DataDoEncontro data;
-    private SingletonDB meusEncontros = SingletonDB.getInstance();
+	private ArrayList<Encontro> notificacoes = new ArrayList<Encontro>();
+	private ArrayAdapter tweetItemArrayAdapter;
+	private HorarioDoEncontro horario;
+	private DataDoEncontro data;
+	private SingletonDB meusEncontros = SingletonDB.getInstance();
 
-    @Override
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-	      //setContentView(R.layout.fragment_meus_encontros);
+		// setContentView(R.layout.fragment_meus_encontros);
 
-    	notificacoes = new ArrayList<Encontro>();
-    	data = new DataDoEncontro(12, 12, 14);
-    	horario = new HorarioDoEncontro(12, 12);
-		View rootView = inflater.inflate(R.layout.fragment_notificacoes, container, false);
-		
-		for (int i = 0 ; i < 10 ;  i++){
-				Encontro encontro = new Encontro("encontro " + String.valueOf(i),  
-						" ponto " + String.valueOf(i),
-						" linha " +   String.valueOf(i),
-						null, 
-						null);
+		data = new DataDoEncontro(12, 12, 14);
+		horario = new HorarioDoEncontro(12, 12);
+		View rootView = inflater.inflate(R.layout.fragment_notificacoes,
+				container, false);
+
+		for (int i = 0; i < 10; i++) {
+			Encontro encontro = new Encontro("encontro " + String.valueOf(i),
+					" ponto " + String.valueOf(i), " linha "
+							+ String.valueOf(i), null, null);
+//			if (!meusEncontros.contem(notificacoes.get(i)))
 				notificacoes.add(encontro);
 		}
-		
-		
-	       
-	        
-	      tweetItemArrayAdapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, notificacoes);
-	      tweetListView = (ListView) rootView.findViewById(R.id.notificacoes);
-	      tweetListView.setAdapter(tweetItemArrayAdapter);
-	        
-	      tweetListView= (ListView) rootView.findViewById(R.id.notificacoes);
-	      tweetListView.setOnItemClickListener(new OnItemClickListener() {
 
+		tweetItemArrayAdapter = new ArrayAdapter(this.getActivity(),
+				android.R.layout.simple_list_item_1, notificacoes);
+		tweetListView = (ListView) rootView.findViewById(R.id.notificacoes);
+		tweetListView.setAdapter(tweetItemArrayAdapter);
 
-	    		  @Override
-	    		  public void onItemClick(AdapterView<?> parent, View view,
-	    		    int position, long id) {
-//	    			  ImageView imageView = (ImageView) view.findViewById(R.id.icon);
-	    			  String situacao;
-	    			  if (meusEncontros.contem(notificacoes.get(position))){
-	    				  meusEncontros.delEncontro(notificacoes.get(position));
-	    				  situacao = "removido";
-	    			  }
-	    			  else {
-	    				  meusEncontros.addEncontro(notificacoes.get(position));
-	    				  situacao = "adicionado";
-	    			  }
-	    		    Toast.makeText(getActivity().getApplicationContext(),
-	    		      "Encontro " + position + " " +  situacao, Toast.LENGTH_LONG)
-	    		      .show();
-	    		    tweetItemArrayAdapter.notifyDataSetChanged();
-	    		  }
-	    		}); 
+		tweetListView = (ListView) rootView.findViewById(R.id.notificacoes);
+		tweetListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				String situacao = "";
+				final int pos = position;
+				// ImageView imageView = (ImageView)
+				// view.findViewById(R.id.icon);
+
+				AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+						.create(); // Read Update
+				alertDialog.setTitle(notificacoes.get(position).getNome());
+				alertDialog
+						.setMessage(notificacoes.get(position).getPonto()
+								+ "\n"
+								+ notificacoes.get(position).getLinha()
+								+ "\n"
+								+ notificacoes.get(position).getData()
+								+ "\n"
+								+ notificacoes.get(position).getHorario()
+								+ "\n"
+								+ "confirmados: \n"
+								+ "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.");
+
+				alertDialog.setButton("Confirmar",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								meusEncontros.addEncontro(notificacoes.get(pos));
+								notificacoes.remove(pos);
+								Toast.makeText(
+										getActivity().getApplicationContext(),
+										"Encontro " + pos + " adicionado",
+										Toast.LENGTH_LONG).show();
+								tweetItemArrayAdapter.notifyDataSetChanged();
+
+							}
+						});
+				alertDialog.setButton2("Fechar",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// simplesmente sai da janela
+							}
+						});
+
+				alertDialog.show();
+
+				// if (meusEncontros.contem(notificacoes.get(position))){
+				// meusEncontros.delEncontro(notificacoes.get(position));
+				// situacao = "removido";
+				// }
+				// else {
+				// meusEncontros.addEncontro(notificacoes.get(position));
+				// situacao = "adicionado";
+				// }
+				// Toast.makeText(getActivity().getApplicationContext(),
+				// "Encontro " + position + " " + situacao, Toast.LENGTH_LONG)
+				// .show();
+				tweetItemArrayAdapter.notifyDataSetChanged();
+			}
+		});
 		return rootView;
 	}
 }

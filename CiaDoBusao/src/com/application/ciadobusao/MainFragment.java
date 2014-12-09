6cf5d59a286c442a8e39d88504f58f99002e7087
CelioBarros <celio.barros.filho@gmail.com>
2014-12-09 01:27:15 -0300

    Modificação

--- CiaDoBusao/src/com/application/ciadobusao/MainFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/MainFragment.java
@@ -1,5 +1,7 @@
 package com.application.ciadobusao;
 
+import java.util.Arrays;
+
 import com.application.ciadobusao.telas.MenuActivity;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -28,6 +30,7 @@ public class MainFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	    View view = inflater.inflate(R.layout.activity_main, container, false);
 	    LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+	    authButton.setReadPermissions(Arrays.asList("public_profile"));
 	    authButton.setFragment(this);
 	    inicioButton = (Button) view.findViewById(R.id.buttonInicio);
 	    visivel = View.VISIBLE;

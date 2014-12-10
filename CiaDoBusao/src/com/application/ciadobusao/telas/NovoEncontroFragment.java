f4acbd16ae53320e5a072cdc12dc9b449328636e
rennanhm <rennanhm@gmail.com>
2014-12-10 02:16:22 -0300

    Verificacao de entrada valida

--- CiaDoBusao/src/com/application/ciadobusao/telas/NovoEncontroFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/NovoEncontroFragment.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.text.format.DateFormat;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -33,7 +36,11 @@ public class NovoEncontroFragment extends Fragment{
 	private DataDoEncontro data;
 	private HorarioDoEncontro horario;
 	private Encontro encontro; 
+	
 	private View rootView;
+	private TextView nomeEncontroTextView;
+	private TextView linhaEncontroTextView;
+	private TextView pontoEncontroTextView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +48,14 @@ public class NovoEncontroFragment extends Fragment{
 
 		rootView = inflater.inflate(R.layout.fragment_novo_encontro, container, false);
 
+		nomeEncontroTextView = (TextView) rootView.findViewById(R.id.nomeEdit);
+		linhaEncontroTextView = (TextView) rootView.findViewById(R.id.linhaEdit);
+		pontoEncontroTextView = (TextView) rootView.findViewById(R.id.pontoEdit);
+		
+		
+		
+	
+		
 		dataButton= (Button) rootView.findViewById(R.id.dataPickerButton);
 		dataButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -65,28 +80,68 @@ public class NovoEncontroFragment extends Fragment{
 		criarEncontroButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				encontro = new Encontro(
-						((TextView) rootView.findViewById(R.id.nomeEdit)).getText().toString(),
-						((TextView) rootView.findViewById(R.id.pontoEdit)).getText().toString(),
-						((TextView) rootView.findViewById(R.id.linhaEdit)).getText().toString(),
-						horario, 
-						data);
-				SingletonDB.getInstance().addEncontro(encontro);
-				FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-				fragmentManager.beginTransaction()
-						.replace(R.id.container, new HomeFragment()).commit();
-				Toast.makeText(getActivity(), "Encontro Criado",Toast.LENGTH_LONG).show();
+				String nomeEncontro = ((TextView) rootView.findViewById(R.id.nomeEdit)).getText().toString();
+				String pontoEncontro = ((TextView) rootView.findViewById(R.id.pontoEdit)).getText().toString();
+				String linhaEncontro = ((TextView) rootView.findViewById(R.id.linhaEdit)).getText().toString();
+				int count = 0;
+				if(!isNomeEncontroValid(nomeEncontro)){
+						nomeEncontroTextView.setError("Nome Invalido");
+						count = count +1;
+				}
+				if(!isPontoEcontroValid(pontoEncontro)){
+					pontoEncontroTextView.setError("Ponto Invalido");
+					count = count +1;
+				}
+				if(!isLinhaEncontroValid(linhaEncontro)){
+					linhaEncontroTextView.setError("Linha Invalida");
+					count = count +1;
+				}
+				
+				if(count==0){
+				
+					encontro = new Encontro(
+							nomeEncontro,
+							pontoEncontro,
+							linhaEncontro,
+							horario, 
+							data);
+					SingletonDB.getInstance().addEncontro(encontro);
+					FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+					fragmentManager.beginTransaction()
+							.replace(R.id.container, new HomeFragment()).commit();
+					Toast.makeText(getActivity(), "Encontro Criado",Toast.LENGTH_LONG).show();
+				
+				
+				}
 			   
 			}
 		});
 		return rootView;
 	}	
+	
+	private boolean isPontoEcontroValid(String ponto){
+		if(ponto.length()>2  && ponto != null){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isLinhaEncontroValid(String linha){
+		if(linha.length()>2 && linha != null){
+			return true;
+		}
+		return false;
+	}
+	private boolean isNomeEncontroValid(String nomeEncontro){
+		if(nomeEncontro.length()>3 && nomeEncontro != null){
+			return true;
+		}
+		return false;
+	}
 
 
 
-
-
-
+	 
 
 	//=======================DATA PICKER =====================
 	public class DatePickerFragment extends DialogFragment

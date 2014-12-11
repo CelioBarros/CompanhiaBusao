72f113102b1028acf154758a139c494657698e07
leonardoavelino <leonardoavelino@gmail.com>
2014-12-11 01:19:45 -0300

    Refatorada o PerfilFragment.

--- CiaDoBusao/src/com/application/ciadobusao/telas/PerfilFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/PerfilFragment.java
@@ -15,126 +15,117 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-
 public class PerfilFragment extends Fragment {
-	
+
 	private static final int REAUTH_ACTIVITY_CODE = 100;
 	private ProfilePictureView profilePictureView;
-	private TextView profileSex;
 	private View view;
 	protected TextView userNameView;
 	protected TextView userNameGender;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    uiHelper = new UiLifecycleHelper(getActivity(), callback);
-	    uiHelper.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		// Check for an open session
-	    
-	
-		view = inflater.inflate(R.layout.fragment_perfil,container, false);
-		
-		profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+		profilePictureView = (ProfilePictureView) view
+				.findViewById(R.id.selection_profile_pic);
 		profilePictureView.setCropped(true);
 		userNameView = (TextView) view.findViewById(R.id.selection_user_name);
 		userNameGender = (TextView) view.findViewById(R.id.selection_gender);
 		Session session = Session.getActiveSession();
-	    if (session != null && session.isOpened()) {
-	        // Get the user's data
-	        makeMeRequest(session);
-	    }
-		
-		
+		if (session != null && session.isOpened()) {
+			makeMeRequest(session);
+		}
+
 		return view;
 	}
-	
+
 	private void makeMeRequest(final Session session) {
-	    // Make an API call to get user data and define a 
-	    // new callback to handle the response.
-	    Request request = Request.newMeRequest(session, 
-	            new Request.GraphUserCallback() {
-	        @Override
-	        public void onCompleted(GraphUser user, Response response) {
-	            // If the response is successful
-	            if (session == Session.getActiveSession()) {
-	                if (user != null) {
-	                    // Set the id for the ProfilePictureView
-	                    // view that in turn displays the profile picture.
-	                    profilePictureView.setProfileId(user.getId());
-	                    // Set the Textview's text to the user's name.
-	                    userNameView.setText(user.getName());
-	                    String gender = "";
-	                    if (user.asMap().get("gender").toString().equals("male")) {
-							gender = "Masculino";
-						} else if (user.asMap().get("gender").toString().equals("female")) {
-							gender = "Feminino";
-						} else {
-							gender = "Indefinido";
+		Request request = Request.newMeRequest(session,
+				new Request.GraphUserCallback() {
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if (session == Session.getActiveSession()) {
+							if (user != null) {
+								profilePictureView.setProfileId(user.getId());
+								userNameView.setText(user.getName());
+								String gender = "";
+								if (user.asMap().get("gender").toString()
+										.equals("male")) {
+									gender = "Masculino";
+								} else if (user.asMap().get("gender")
+										.toString().equals("female")) {
+									gender = "Feminino";
+								} else {
+									gender = "Indefinido";
+								}
+								userNameGender.setText("Gênero: " + gender);
+							}
 						}
-	                    userNameGender.setText("GÃªnero: " + gender);
-	                }
-	            }
-	            if (response.getError() != null) {
-	                // Handle errors, will do so later.
-	            }
-	        }
-	    });
-	    request.executeAsync();
+						if (response.getError() != null) {
+						}
+					}
+				});
+		request.executeAsync();
 	}
-	
-	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
-	    if (session != null && session.isOpened()) {
-	        // Get the user's data.
-	        makeMeRequest(session);
-	    }
+
+	private void onSessionStateChange(final Session session,
+			SessionState state, Exception exception) {
+		if (session != null && session.isOpened()) {
+			makeMeRequest(session);
+		}
 	}
-	
+
 	private UiLifecycleHelper uiHelper;
 	private Session.StatusCallback callback = new Session.StatusCallback() {
-	    @Override
-	    public void call(final Session session, final SessionState state, final Exception exception) {
-	        onSessionStateChange(session, state, exception);
-	    }
+		@Override
+		public void call(final Session session, final SessionState state,
+				final Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
 	};
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    super.onActivityResult(requestCode, resultCode, data);
-	    if (requestCode == REAUTH_ACTIVITY_CODE) {
-	        uiHelper.onActivityResult(requestCode, resultCode, data);
-	    }
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REAUTH_ACTIVITY_CODE) {
+			uiHelper.onActivityResult(requestCode, resultCode, data);
+		}
 	}
-	
+
 	@Override
 	public void onResume() {
-	    super.onResume();
-	    uiHelper.onResume();
+		super.onResume();
+		uiHelper.onResume();
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle bundle) {
-	    super.onSaveInstanceState(bundle);
-	    uiHelper.onSaveInstanceState(bundle);
+		super.onSaveInstanceState(bundle);
+		uiHelper.onSaveInstanceState(bundle);
 	}
 
 	@Override
 	public void onPause() {
-	    super.onPause();
-	    uiHelper.onPause();
+		super.onPause();
+		uiHelper.onPause();
 	}
 
 	@Override
 	public void onDestroy() {
-	    super.onDestroy();
-	    uiHelper.onDestroy();
+		super.onDestroy();
+		uiHelper.onDestroy();
 	}
 
 }

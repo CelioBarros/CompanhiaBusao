505822413a6d0735028e2b2352cd7eb83d85aad7
leonardoavelino <leonardoavelino@gmail.com>
2015-01-26 02:48:23 -0300

    Funcionando deletar encontro. Aplicativo agora não é rotacionado pela inclinação do dispositivo.

--- CiaDoBusao/src/com/application/ciadobusao/telas/MeusEncontrosFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/MeusEncontrosFragment.java
@@ -67,17 +67,18 @@ public class MeusEncontrosFragment extends Fragment {
 						+ "\n"
 						+ item.getHorario().toString()
 						+ "\n"
-						+ item.getPerfisConfirmados()
-						+ "\n"
-						+ "Mussum ipsums cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
-						+ "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.");
+						+ item.getPerfisConfirmados());
 
 				alertDialog.setButton("Desconfirmar",
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int which) {
-								newRest.desconfirmarPresenca(auxMeusEncontros.get(pos).getId(), PerfilFragment.getUser().getName());
-								new MeuAsyncTask().execute();
+								if (auxMeusEncontros.get(pos).getIdDono().equals(PerfilFragment.getUser().getId())) {
+									newRest.deletarEncontro(auxMeusEncontros.get(pos).getId());
+								} else {
+									newRest.desconfirmarPresenca(auxMeusEncontros.get(pos).getId(), PerfilFragment.getUser().getName());
+									new MeuAsyncTask().execute();
+								}
 							}
 						});
 				alertDialog.setButton2("Fechar",

c244fdb9eb9dcd4c1a6ce28b5b63c41536dcc3ad
leonardoavelino <leonardoavelino@gmail.com>
2015-01-26 01:27:01 -0300

    Confirmação de presença no encontro. (faltou no ultimo commit)

--- CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
@@ -45,13 +45,14 @@ public class NotificacoesFragment extends Fragment {
 		
 		new MeuAsyncTask().execute();
 
+		final ClienteRest newRest = new ClienteRest();
 		
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
 			@SuppressWarnings("deprecation")
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
+					final int position, long id) {
 				String situacao = "";
 				final int pos = position;
 				// ImageView imageView = (ImageView)
@@ -69,14 +70,14 @@ public class NotificacoesFragment extends Fragment {
 								+ "\n"
 								+ listaEncontros.get(position).getHorario()
 								+ "\n"
-								+ "confirmados: \n"
-								+ "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
-								+ "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.");
+								+ "confirmados: " + listaEncontros.get(position).getPerfisConfirmados());
 
 				alertDialog.setButton("Confirmar",
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int which) {
+								newRest.confirmaPresenca(listaEncontros.get(position).getId(), PerfilFragment.getUser().getName());
+								new MeuAsyncTask().execute();
 								//meusEncontros.addEncontro(notificacoes.getNotificacoes().get(pos));
 								///notificacoes.delNotificacaoAtIndex(pos);
 								//Toast.makeText(

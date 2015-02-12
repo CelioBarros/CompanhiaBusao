0c2f69d0c3df7e31e884de65842bf22e72ade4aa
leonardoavelino <leonardoavelino@gmail.com>
2015-02-12 01:46:36 -0300

    Pegando agora a localização no mapa através de um botão.

--- CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
+++ CiaDoBusao/src/com/application/ciadobusao/telas/NotificacoesFragment.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import java.util.List;
 
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,13 +15,15 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 import com.application.ciadobusao.R;
 import com.application.ciadobusao.db.ClienteRest;
 import com.application.ciadobusao.util.AdapterListNotificacoesView;
 import com.application.ciadobusao.util.CheckNetwork;
 import com.application.ciadobusao.util.Encontro;
+import com.application.ciadobusao.util.MyLocation;
+import com.application.ciadobusao.util.MyLocation.LocationResult;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class NotificacoesFragment extends Fragment {
@@ -28,26 +31,56 @@ public class NotificacoesFragment extends Fragment {
 	private ListView listView;
 	private List<String> listaNotificacoes = new ArrayList<String>();
 	private List<Encontro> listaEncontros;
+	private View rootView;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_map,
-				container, false);
 		
-		SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
-		 
-		 GoogleMap map = mapFragment.getMap();
-		       
-		 map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
+		SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+				.findFragmentById(R.id.map);
+
+		final GoogleMap map = mapFragment.getMap();
+
+		map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+		LocationResult locationResult = new LocationResult() {
+			@Override
+			public void gotLocation(Location location) {
+				map.setMyLocationEnabled(true);
+			}
+		};
+		MyLocation myLocation = new MyLocation();
+		myLocation.getLocation(getActivity(), locationResult);
 
 		if (!CheckNetwork.isInternetAvailable(getActivity())) {
-			Toast.makeText(getActivity(),"Verifique sua conexão com a Internet e tente novamente.",1500).show();
-		} 
+			Toast.makeText(getActivity(),
+					"Verifique sua conexão com a Internet e tente novamente.",
+					1500).show();
+		}
 		return rootView;
 	}
 	
-	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
 
+	@Override
+	public void onSaveInstanceState(Bundle bundle) {
+		super.onSaveInstanceState(bundle);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+	
 }
